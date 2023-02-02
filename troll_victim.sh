3ff2b074805fe6790f4990c38aff9b9eff727d5f
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-02-03 01:39:23 +0600

    first commit v0.1

--- troll_victim.sh
+++ troll_victim.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# This script changes linux desktop's
+# plug in & out system sound effect. 
+# Nothing dangerous, just trolling the victim.
+
+# Author: Mr. Garbage
+# Version: 0.1
 
 function setSystemSoundEffect() {
-    sudo cp 
+    # Download audio files
+    cd /tmp
+    wget https://github.com/sheikhshakibhossain/leaveTraceLinux/raw/main/sound/yeh_ma_ka_bhosra.ogg
+    wget https://github.com/sheikhshakibhossain/leaveTraceLinux/raw/main/sound/anime_girl_moan.ogg
+    
+    # set them as system audio
+    sudo cp anime_girl_moan.ogg /usr/share/sounds/freedesktop/stereo/power-plug.oga
+    sudo cp yeh_ma_ka_bhosra.ogg /usr/share/sounds/freedesktop/stereo/power-unplug.oga
 }
+
+
+# execution begins main()
+setSystemSoundEffect
+exit
