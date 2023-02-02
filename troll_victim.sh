87574d456843b188ab593278945c801fe31e7b61
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-02-03 01:53:15 +0600

    first commit v0.1

--- troll_victim.sh
+++ troll_victim.sh
@@ -12,10 +12,12 @@ function setSystemSoundEffect() {
     cd /tmp
     wget https://github.com/sheikhshakibhossain/leaveTraceLinux/raw/main/sound/yeh_ma_ka_bhosra.ogg
     wget https://github.com/sheikhshakibhossain/leaveTraceLinux/raw/main/sound/anime_girl_moan.ogg
+    clear
     
     # set them as system audio
     sudo cp anime_girl_moan.ogg /usr/share/sounds/freedesktop/stereo/power-plug.oga
     sudo cp yeh_ma_ka_bhosra.ogg /usr/share/sounds/freedesktop/stereo/power-unplug.oga
+    echo "Done! System Sound Effect has been changed!"
 }
 
 
